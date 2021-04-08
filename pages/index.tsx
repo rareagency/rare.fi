999ac7718393832192047fb800f82feb269d9342
Oskari Kuikka <oskari@rare.fi>
2021-04-08 10:35:16 +0300

    Added ESLint and Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Rare.fi">
-    <h1>Rare.fi</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout title="Home | Rare.fi">
+      <h1>Rare.fi</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
