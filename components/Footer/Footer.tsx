bba9a6d249436c8c7230ceda0e2de025803930fc
Oskari Kuikka <oskari@rare.fi>
2021-04-12 10:51:26 +0300

    Added logo and refactored Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,17 @@
 import React from "react";
+import Logo from "../Logo";
 import FooterItem from "../Footer/FooterItem";
 import { links } from "../../utils/pagelinks";
 
 const Footer = () => {
   return (
     <footer className="bg-black">
-      <div className="w-100 mx-auto h-144 text-white pl-24 pr-24 pt-16">
-        <div className="p-16 pl-16 pb-28 h-full">
+      <div className="w-100 mx-auto h-144 text-white pl-24 pr-24 pt-16 lg:pl-4 lg:pr-4">
+        <div className="p-16 pb-28 h-full ">
           <div className="flex flex-grow-1 justify-between gap-16 h-full">
             <div className="relative">
-              <div className="text-6xl">
-                <div>RARE</div>
-              </div>
-              <div className="absolute bottom-0 inset-x-0">
+              <Logo />
+              <address className="absolute bottom-0 inset-x-0">
                 <div className="font-bold pt-16">Tampere</div>
                 <div className="pt-8">
                   <div>Polttimonkatu 4</div>
@@ -23,7 +22,7 @@ const Footer = () => {
                   <div>+358 50 470 0715</div>
                   <div>riku@rare.fi</div>
                 </div>
-              </div>
+              </address>
             </div>
             <div className="text-xl flex flex-col-reverse">
               <div>3092335-9</div>
