8166587c43bce1a82fc5ac9816e14b351e244a00
Oskari Kuikka <oskari@rare.fi>
2021-04-13 20:25:25 +0300

    Updated href target

--- components/References.tsx
+++ components/References.tsx
@@ -7,36 +7,36 @@ const References = () => {
         Proudly delivered digital products with
       </div>
       <div className="grid grid-cols-4 gap-x-16 gap-y-8 p-4 md:grid-cols-2">
-        <a href="https://www.tieto.com/fi/" target="blank">
+        <a href="https://www.tieto.com/fi/" target="_blank">
           <img src="/static/references/tieto.svg" alt="Tieto Oyj" />
         </a>
-        <a href="https://www.simucu.be/" target="blank">
+        <a href="https://www.simucu.be/" target="_blank">
           <img src="/static/references/simucube.svg" alt="Simucube" />
         </a>
-        <a href="https://instagrid.co" target="blank">
+        <a href="https://instagrid.co" target="_blank">
           <img
             src="/static/references/instagrid.svg"
             className="w-[150px] sm:w-[140px]"
             alt="Instagrid"
           />
         </a>
-        <a href="https://plan-international.org/" target="blank">
+        <a href="https://plan-international.org/" target="_blank">
           <img src="/static/references/plan.svg" alt="Plan International" />
         </a>
-        <a href="https://linnunrata.fi" target="blank">
+        <a href="https://linnunrata.fi" target="_blank">
           <img src="/static/references/linnunrata.svg" alt="Linnunrata" />
         </a>
-        <a href="https://cultsecurity.com/" target="blank">
+        <a href="https://cultsecurity.com/" target="_blank">
           <img
             src="/static/references/cult-black.png"
             className="w-[150px]"
             alt="Cult Security"
           />
         </a>
-        <a href="https://sharpershape.com/" target="blank">
+        <a href="https://sharpershape.com/" target="_blank">
           <img src="/static/references/sharper-shape.svg" alt="Sharper Shape" />
         </a>
-        <a href="https://www.veracell.com/" target="blank">
+        <a href="https://www.veracell.com/" target="_blank">
           <img src="/static/references/veracell.svg" alt="Veracell" />
         </a>
       </div>
