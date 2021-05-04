af6d69e76b2b8933bb3448ee59c238fef46bc75a
Oskari Kuikka <oskari@rare.fi>
2021-05-04 14:54:17 +0300

    Updated Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -18,21 +18,23 @@ const Footer = () => {
   return (
     <footer className={container}>
       <section className="col-start-2 col-span-5 md:col-start-1">
-        <div className="text-2xl pb-2">Software made</div>
-        <div className="text-6xl font-mono">collaborative</div>
+        <div className="text-2xl pb-2 font-AltoneMedium">Software made</div>
+        <div className="text-6xl font-TTAutonomousMonoRegular">
+          collaborative
+        </div>
       </section>
 
       <ol className="col-start-7 row-start-1 md:row-start-2 md:col-start-1 md:col-span-3">
         {links.map((link) => (
-          <li className="p-2 text-right font-bold md:text-left">
+          <li className="p-2 text-right font-AltoneBold md:text-left">
             <FooterItem url={link.url} text={link.text} key={link.text} />
           </li>
         ))}
       </ol>
 
-      <address className="space-y-6 col-start-2 row-start-2 font-mono pb-24 pt-24 uppercase md:row-start-3 md:col-start-1">
+      <address className="space-y-6 col-start-2 row-start-2 pb-24 pt-24 uppercase md:row-start-3 md:col-start-1 md:col-span-2 font-TTAutonomousMonoBold">
         <ol className="pt-16">
-          <li className="font-bold pb-6">Tampere</li>
+          <li className="pb-6">Tampere</li>
           <li>Polttimonkatu 4</li>
           <li>33210 Tampere</li>
           <li>Finland</li>
@@ -47,13 +49,13 @@ const Footer = () => {
         </ul>
       </address>
       <div className="layout-grid row-start-3 col-all uppercase text-sm md:text-left md:row-start-4">
-        <div className="col-start-2 col-span-2 font-mono md:row-start-1 md:col-start-1 md:col-span-3">
+        <div className="col-start-2 col-span-2 md:row-start-1 md:col-start-1 md:col-span-3">
           (c) copyright rare agency
         </div>
-        <div className="col-span-2 text-center font-mono md:row-start-2 md:col-span-3 md:text-left">
+        <div className="col-span-2 text-center md:row-start-2 md:col-span-3 md:text-left">
           🇫🇮 Made in Finland
         </div>
-        <div className="col-span-2 text-right font-bold md:row-start-3 md:col-span-3 md:text-left">
+        <div className="col-span-2 text-right  md:row-start-3 md:col-span-3 md:text-left">
           <a className="button-underline">Cookie & Privacy policy</a>
         </div>
       </div>
