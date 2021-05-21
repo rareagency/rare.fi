b04caf3d0583280aa2c0a873c8169ba767f0cc5c
Pyry Rouvila <pyry.rouvila@gmail.com>
2021-05-21 12:56:22 +0300

    Implement featured story

--- components/Stories/FeaturedStory.tsx
+++ components/Stories/FeaturedStory.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
 const FeaturedStory = () => (
-  <section id="featured-story" className="col-start-2 col-span-4 mt-12">
+  <article id="featured-story" className="col-start-2 col-span-6 mt-12">
     <h2 className="text-4xl leading-relaxed text-dark-blue pt-6 pb-6">
       What I learned from building
       <br />a Telegram bot on Next.js
     </h2>
-  </section>
+
+    <p className="flex items-center">
+      <img src="/static/icons/fresh.svg" alt="Fresh" />
+      <span className="tag">
+        <b className="tag font-bold">Practical guide</b> by Riku Rouvila
+      </span>
+    </p>
+
+    <div className="relative h-[425px] mt-12 mb-12">
+      <Image
+        src="/static/featured-article.png"
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  </article>
 );
 
 export default FeaturedStory;
