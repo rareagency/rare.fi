03c532e9059cd518ec4cc3b69adb2ed9f87515a9
Oskari Kuikka <oskari@rare.fi>
2021-05-21 13:49:04 +0300

    Button hover and refactor

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,42 +1,34 @@
 import React from "react";
-import cntl from "cntl";
 
 type ButtonProps = {
   kind?: "primary" | "secondary";
   className?: string;
 };
 
-const buttonClass = ({ kind = "primary", className }: ButtonProps) => cntl`
-  ${
-    kind === "primary"
-      ? cntl`
-      bg-action-purple
-      text-off-white
-    `
-      : cntl`
-      bg-off-white
-      text-action-purple
-    `
-  }
-
-  w-auto
-  font-default
-  font-bold
-  text-lg
-  border-solid
-  border-action-purple
-  border-[3px]
-  rounded-full
-  pl-8
-  pr-8
-  pt-2
-  pb-2
-  uppercase
-  ${className}
-`;
-
-const Button: React.FC<ButtonProps> = ({ children, kind, className }) => (
-  <button className={buttonClass({ kind, className })}>{children}</button>
-);
+const Button: React.FC<ButtonProps> = ({
+  children,
+  kind = "primary",
+  className,
+}) => {
+  return (
+    <button className={`${className} button ${kind}`}>
+      <style jsx>{`
+        .button {
+          @apply w-auto font-default font-bold text-lg border-solid border-action-purple border-[3px] rounded-full pl-8 pr-8 pt-2 pb-2 uppercase;
+        }
+        .primary {
+          @apply bg-action-purple text-off-white;
+        }
+        .secondary {
+          @apply bg-off-white text-action-purple;
+        }
+        .button:hover {
+          box-shadow: 0px 6px 0px -3px #ffffff, 0px 7px 0px #1941aa;
+        }
+      `}</style>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
