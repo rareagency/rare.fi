db05eac1a8afce0de03713697d520492a7cb2343
Pyry Rouvila <pyry.rouvila@gmail.com>
2021-05-27 21:42:26 +0300

    Add Rare Est. image

--- components/Ingress.tsx
+++ components/Ingress.tsx
@@ -6,6 +6,21 @@ const Ingress = () => (
       We insert sustainable digital development practives to enable product team
       excellence and technological growth that brings you ahead of the curve
     </span>
+
+    <div className="col-start-2 col-span-2 relative">
+      <img
+        src="/static/rare_est.svg"
+        alt="Rare Est. 2019"
+        className="rare-est"
+      />
+    </div>
+
+    <style jsx>{`
+      .rare-est {
+        position: absolute;
+        bottom: -50%;
+      }
+    `}</style>
   </section>
 );
 
