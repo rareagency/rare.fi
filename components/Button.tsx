487c5abf18a6bbad26cb1235dc09353ddb460b92
Oskari Kuikka <oskari@rare.fi>
2021-05-28 09:42:37 +0300

    Button animation

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,6 +15,13 @@ const Button: React.FC<ButtonProps> = ({
       <style jsx>{`
         .button {
           @apply w-auto font-default font-bold text-lg border-solid border-action-purple border-[3px] rounded-full pl-8 pr-8 pt-2 pb-2 uppercase;
+          animation-name: moveReverse;
+          animation-duration: 0.1s;
+        }
+        .button:hover {
+          animation-name: move;
+          animation-duration: 0.25s;
+          animation-fill-mode: forwards;
         }
         .primary {
           @apply bg-action-purple text-off-white;
@@ -22,8 +29,22 @@ const Button: React.FC<ButtonProps> = ({
         .secondary {
           @apply bg-off-white text-action-purple;
         }
-        .button:hover {
-          box-shadow: 0px 6px 0px -3px #ffffff, 0px 7px 0px #1941aa;
+
+        @keyframes move {
+          from {
+            box-shadow: 0px 0px 0px 0px #ffffff, 0px 0px 0px #1941aa;
+          }
+          to {
+            box-shadow: 0px 7px 0px -4px #ffffff, 0px 6px 0px #1941aa;
+          }
+        }
+        @keyframes moveReverse {
+          from {
+            box-shadow: 0px 7px 0px -4px #ffffff, 0px 6px 0px #1941aa;
+          }
+          to {
+            box-shadow: 0px 0px 0px 0px #ffffff, 0px 0px 0px #1941aa;
+          }
         }
       `}</style>
       {children}
