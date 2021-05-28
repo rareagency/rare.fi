6a66326920ffbd19614317910aa08367384297ec
Oskari Kuikka <oskari@rare.fi>
2021-05-28 14:36:11 +0300

    Added animation

--- components/Floater.tsx
+++ components/Floater.tsx
@@ -7,8 +7,22 @@ const Floater = () => {
     return null;
   }
   return (
-    <section className="bg-action-purple w-full p-4 layout-grid col-all sticky bottom-0">
-      <p className="col-start-2 col-span-4 md:col-span-4 normal-case text-off-white tag md:text-[18px] text-[32px] my-2">
+    <section className="bg-action-purple w-full p-4 layout-grid col-all sticky bottom-0 animation">
+      <style jsx>{`
+        .animation {
+          animation-name: move;
+          animation-duration: 1s;
+        }
+        @keyframes move {
+          from {
+            height: 0px;
+          }
+          to {
+            height: 93px;
+          }
+        }
+      `}</style>
+      <p className="col-start-2 col-span-4 md:col-span-4 normal-case text-off-white md:text-[18px] text-[32px] my-2 tag">
         Psst... Don't miss out!
       </p>
       <Button
