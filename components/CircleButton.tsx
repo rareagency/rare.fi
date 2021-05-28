67162fbc4444cf4b64f731c23dfad941ab9dc463
Oskari Kuikka <oskari@rare.fi>
2021-05-28 14:12:37 +0300

    Updated arrow button to have animation

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -5,28 +5,75 @@ import { c } from "../utils/classnames";
 type CircleButtonProps = {
   link?: string;
   className?: string;
+  text?: string;
 };
 
 const CircleButton: React.FC<CircleButtonProps> = ({
   link,
   className,
-  children,
+  children = <p>Click me! Me Me!</p>,
 }) => {
+  const [isShown, setIsShown] = React.useState(false);
   if (!link) link = "#";
   return (
-    <Link href={link}>
-      <a className="inline-flex items-center ml-12 md:ml-0">
-        <span className="h5 text-action-purple inline-block">{children}</span>
-        <img
-          src="/static/button.svg"
-          alt="Circle button"
-          className={c(
-            "cursor-pointer w-12 inline-block ml-6 md:ml-0",
-            className
+    <div
+      id="button-container"
+      onMouseEnter={() => setTimeout(() => setIsShown(true), 250)}
+      onMouseLeave={() => setTimeout(() => setIsShown(false), 150)}
+    >
+      <Link href={link}>
+        <a
+          className={`inline-flex items-center ml-12 md:ml-0 ${c(className)} `}
+        >
+          <style jsx>{`
+            .text {
+              display: inline-block;
+              @apply rounded-full border-action-purple
+              border-[3px] py-3 px-6;
+            }
+            .img {
+              animation-name: move;
+              animation-duration: 0.3s;
+              @apply rounded-full border-action-purple border-[3px] h-14 w-14 flex items-center justify-center;
+            }
+            .button {
+              animation-name: moveReverse;
+              animation-duration: 0.2s;
+              @apply font-default text-lg border-solid border-action-purple border-[3px] rounded-full px-8 py-2 uppercase;
+            }
+
+            @keyframes move {
+              from {
+                padding-left: 32px;
+                padding-right: 32px;
+              }
+              to {
+                padding-left: 0px;
+                padding-right: 0px;
+              }
+            }
+            @keyframes moveReverse {
+              from {
+                padding-left: 0px;
+                padding-right: 0px;
+              }
+              to {
+                padding-left: 32px;
+                padding-right: 32px;
+              }
+            }
+          `}</style>
+          {!isShown && (
+            <div className="img text-action-purple text-bold">{">"}</div>
           )}
-        />
-      </a>
-    </Link>
+          {isShown && (
+            <button className="button">
+              <span className="h5 text-action-purple">{children}</span>
+            </button>
+          )}
+        </a>
+      </Link>
+    </div>
   );
 };
 
