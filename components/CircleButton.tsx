3457f3a3acda20f7bcf27868009aa743d8d22c1e
Oskari Kuikka <oskari@rare.fi>
2021-06-04 12:10:32 +0300

    Updated CircleButton animation

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -11,67 +11,64 @@ type CircleButtonProps = {
 const CircleButton: React.FC<CircleButtonProps> = ({
   link,
   className,
-  children = <p>Click me!</p>,
+  children = "Click me!",
 }) => {
-  const [isShown, setIsShown] = React.useState(false);
   if (!link) link = "#";
   return (
-    <div
-      id="button-container"
-      onMouseEnter={() => setTimeout(() => setIsShown(true), 150)}
-      onMouseLeave={() => setTimeout(() => setIsShown(false), 250)}
-    >
-      <Link href={link}>
-        <a className={c(`inline-flex items-center ml-12 md:ml-0`, className)}>
-          <style jsx>{`
-            .text {
-              display: inline-block;
-              @apply rounded-full border-action-purple
-              border-[3px] py-3 px-6;
-            }
-            .img {
-              animation-name: move;
-              animation-duration: 0.2s;
-              @apply rounded-full border-action-purple border-[3px] h-14 w-14 flex items-center justify-center;
-            }
-            .button {
-              animation-name: moveReverse;
-              animation-duration: 0.3s;
-              @apply font-default text-lg border-solid border-action-purple border-[3px] rounded-full px-8 py-2 uppercase;
-            }
+    <Link href={link}>
+      <a className={c(`inline-flex items-center ml-12 md:ml-0`, className)}>
+        <style jsx>{`
+          .button {
+            animation-name: move;
+            animation-duration: 0.2s;
+            @apply rounded-full border-action-purple border-[3px] h-14 w-14 flex items-center justify-center;
+          }
+          .button:hover {
+            animation-name: moveReverse;
+            animation-duration: 0.3s;
+            width: 16rem;
+            @apply py-4;
+          }
 
-            @keyframes move {
-              from {
-                padding-left: 32px;
-                padding-right: 32px;
-              }
-              to {
-                padding-left: 0px;
-                padding-right: 0px;
-              }
+          .text:hover > .content {
+            opacity: 1;
+            transition: 0.1s 0.1s;
+          }
+
+          .arrow {
+            position: absolute;
+          }
+
+          .text:hover .arrow {
+            display: none;
+          }
+          .content {
+            opacity: 0;
+          }
+
+          @keyframes move {
+            from {
+              width: 16rem;
             }
-            @keyframes moveReverse {
-              from {
-                padding-left: 0px;
-                padding-right: 0px;
-              }
-              to {
-                padding-left: 32px;
-                padding-right: 32px;
-              }
+            to {
+              width: 3.5rem;
             }
-          `}</style>
-          {!isShown && (
-            <div className="img text-action-purple text-bold">{">"}</div>
-          )}
-          {isShown && (
-            <button className="button">
-              <span className="h5 text-action-purple">{children}</span>
-            </button>
-          )}
-        </a>
-      </Link>
-    </div>
+          }
+          @keyframes moveReverse {
+            from {
+              width: 3.5rem;
+            }
+            to {
+              width: 16rem;
+            }
+          }
+        `}</style>
+        <button className="button text-action-purple text-bold text">
+          <span className="arrow">{">"}</span>
+          <span className="content">{children}</span>
+        </button>
+      </a>
+    </Link>
   );
 };
 
