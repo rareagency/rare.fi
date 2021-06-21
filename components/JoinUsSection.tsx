f18a64b16fddc10fef1740d06fb5631dc5c572d0
Oskari Kuikka <oskari@rare.fi>
2021-06-21 15:26:38 +0300

    Mobile version tweaks

--- components/JoinUsSection.tsx
+++ components/JoinUsSection.tsx
@@ -6,10 +6,13 @@ const JoinUsSection = () => (
     id="join-us"
     className="col-all layout-grid bg-light-yellow pt-16 pb-16"
   >
-    <h2 className="text-action-purple col-start-2 col-span-3 text-4xl leading-relaxed">
+    <h2 className="text-action-purple col-start-2 col-span-3 text-4xl leading-relaxed md:col-span-6 md:col-start-3">
       Rare family welcomes people of any shape and form
     </h2>
-    <Button kind="secondary" className="col-start-2 col-span-2 mt-8">
+    <Button
+      kind="secondary"
+      className="col-start-2 col-span-2 md:col-start-3 md:col-span-4 mt-8"
+    >
       Dare to join Rare
     </Button>
   </section>
