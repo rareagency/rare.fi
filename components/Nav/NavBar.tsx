887013258187eabd1bfe92cf2411ba295b6c253e
Cihan Bebek <cihan.m.bebek@gmail.com>
2021-07-04 13:43:28 +0300

    add black menu option

--- components/Nav/NavBar.tsx
+++ components/Nav/NavBar.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import Logo from "../Logo";
 import NavItem from "./NavItem";
 import Lottie from 'react-lottie';
-import * as animationData from './lottie-menu-icon.json'
+import * as menuAnimationWhite from './lottie-menu-icon.json'
+import * as menuAnimationBlack from './lottie-menu-icon-black.json'
 import { links } from "../../utils/pagelinks";
 
 const SMALL_MENU_SCROLL_THRESHOLD = 150;
@@ -18,6 +19,17 @@ const NavBar: React.FC<{ theme?: "dark" | "light" }> = ({
   function toggleMenu() {
     setMenuIsOpen(!menuIsOpen);
     setAnimationPaused(false);
+
+    // kinda dirty solution to remove scrolling when menu is open, but it works
+    const bodyElement = document.getElementsByTagName('body')[0];
+
+    if (menuIsOpen) {
+      bodyElement.classList.remove('modal-open');
+      return;
+    }
+
+    bodyElement.classList.add('modal-open');
+    return;
   }
 
   function listenScrollEvent() {
@@ -41,7 +53,7 @@ const NavBar: React.FC<{ theme?: "dark" | "light" }> = ({
     <nav className={`${userIsScrolled && !menuIsOpen ? 'bg-action-purple' : `${theme === 'light' ? 'bg-white' : 'bg-black'}`} w-full layout-grid col-all transition-all duration-300 z-20 fixed px-8`}>
       <div className={`${userIsScrolled && !menuIsOpen ? 'pt-4 pb-4' : 'pt-16 pb-16'} transition-all duration-300 flex items-center justify-between col-start-2 col-end-8`}>
         <Link href="/">
-          <a className="z-30">{theme === "dark" || userIsScrolled ? <Logo kind="light" /> : <Logo kind="dark" />}</a>
+          <a className="z-30">{theme === "dark" || userIsScrolled || menuIsOpen ? <Logo kind="light" /> : <Logo kind="dark" />}</a>
         </Link>
 
         <section className="space-x-16 text-xl inline-flex lg:hidden">
@@ -59,7 +71,7 @@ const NavBar: React.FC<{ theme?: "dark" | "light" }> = ({
 
         <div className="hidden lg:inline-flex z-30" onClick={toggleMenu}>
           <Lottie
-            options={{ animationData, autoplay: false, loop: false }}
+            options={{ animationData: !userIsScrolled && theme === 'light' && !menuIsOpen ? menuAnimationBlack : menuAnimationWhite, autoplay: false, loop: false }}
             height={30}
             width={30}
             isPaused={animationPaused}
@@ -69,7 +81,7 @@ const NavBar: React.FC<{ theme?: "dark" | "light" }> = ({
           />
         </div>
 
-        <div className={`${menuIsOpen ? 'fixed w-full h-full animate-blur-in' : 'hidden'} inset-0 bg-action-purple z-20 overflow-y-auto flex flex-col`}>
+        <div className={`${menuIsOpen ? 'fixed w-full h-screen overflow-hidden animate-blur-in' : 'hidden'} inset-0 bg-action-purple z-20 flex flex-col`}>
           <section className="flex flex-1 flex-col content-start pt-40 px-4">
             {links.map((link) => (
               <div key={link.text} className="pb-10 text-left">
