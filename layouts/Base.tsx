d27921f6cd2a73e7f173a2e63adbde655b923055
Joel Bergman <joel.h.bergman@gmail.com>
2021-08-10 15:46:24 +0300

    Add cookie banner (#30)
    Co-authored-by: Pyry <pyry.rouvila@gmail.com>

--- layouts/Base.tsx
+++ layouts/Base.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import Head from "next/head";
+import React from "react";
+import CookieConsent from "../components/CookieConsent";
 
 const TITLE = "Rare Tampere - 2020-luvun ohjelmistokehitystä";
 
@@ -28,6 +29,8 @@ const Base: React.FC<{ title?: string }> = ({ title, children }) => {
       </Head>
 
       <main className="layout-grid">{children}</main>
+
+      <CookieConsent />
     </>
   );
 };
