64781601f5e64e824eba253030a7df04ec2bf5d9
naftis <pyry.rouvila@gmail.com>
2021-09-20 00:38:42 +0300

    Test scroll animation on header

--- components/Header/LanderHeader.tsx
+++ components/Header/LanderHeader.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { c } from "../../utils/classnames";
+import { useThresholdScroller } from "../../utils/threshold-scroller";
 import NavBar from "../Nav/NavBar";
 
 const Header = () => {
+  const isUserScrolled = useThresholdScroller({ threshold: 500 });
+
   return (
     <header
       id="page-header"
-      className="col-all layout-grid bg-black min-h-screen md:min-h-0"
+      className={c("col-all layout-grid min-h-screen md:min-h-0 ", {
+        "bg-black": !isUserScrolled,
+        "bg-white": isUserScrolled,
+        "lg:bg-black": isUserScrolled,
+      })}
     >
       <NavBar theme="dark" />
-      <div className="col-start-2 col-end-8 relative flex flex-col">
-        <h1 className="text-off-white mt-[34vh] mb-20 xl:p-8 md:mt-[20vh] md:mb-0 break-words">
+      <div className="lander-header col-start-2 col-end-8 relative flex flex-col">
+        <h1
+          className={c(
+            "text-off-white mt-[34vh] mb-20 xl:p-8 md:mt-[20vh] md:mb-0 break-words",
+            {
+              "text-black": isUserScrolled,
+              "lg:text-off-white": isUserScrolled,
+            }
+          )}
+        >
           Everyday
           <br />
           contribution to
@@ -22,6 +38,12 @@ const Header = () => {
           </span>
         </h1>
       </div>
+
+      <style jsx>{`
+        #page-header {
+          transition: all 200ms ease-in-out;
+        }
+      `}</style>
     </header>
   );
 };
