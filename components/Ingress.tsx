444597f0fd37b838e2c90eca4413b7d8b413b975
naftis <pyry.rouvila@gmail.com>
2021-09-20 22:12:18 +0300

    Add right aligned text back to Ingress

--- components/Ingress.tsx
+++ components/Ingress.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Ingress = () => (
   <section id="ingress" className="layout-grid col-all md:bg-black">
-    <span className="ingress mt-32 mb-32 col-start-4 col-span-3 md:col-start-2 md:col-span-6 md:m-8 md:mb-32 md:text-off-white md:min-h-[200px]">
+    <span className="ingress mt-32 mb-32 col-start-4 col-span-3 md:col-start-3 md:col-span-5 md:m-8 md:mb-32 md:text-off-white md:min-h-[200px]">
       We insert sustainable digital development practives to enable product team
       excellence and technological growth that brings you ahead of the curve
     </span>
