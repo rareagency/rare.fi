58ecde8e98affbb2172a4b1ec7022056bf15964f
Pyry Rouvila <pyry.rouvila@gmail.com>
2021-10-22 13:07:10 +0300

    Fix titles overflowing on BlogPostCard on mobile (#56)

--- components/Stories/BlogPostCard.tsx
+++ components/Stories/BlogPostCard.tsx
@@ -15,7 +15,7 @@ const BlogPostCard = ({ title, header, image, imageAlt, href }: StoryProps) => (
     <a className="group block">
       <img src={image} alt={imageAlt} className="mb-6 md:mb-3" />
       <span className="tag text-mid-blue">{title}</span>
-      <span className="h4 mt-6 sm:mt-3 sm:text-[15px] group-hover:text-action-purple">
+      <span className="h4 mt-6 sm:mt-3 sm:text-[14px] group-hover:text-action-purple break-words">
         {header}
       </span>
     </a>
