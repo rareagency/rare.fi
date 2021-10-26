efd0e1633cfdaf6b95c272ddb936a1cd134f610b
Marko Leinikka <marko@rare.fi>
2021-10-26 14:19:20 +0300

    Add posts.json to /blog too

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,6 +4,7 @@ import BlogPosts from "../../components/Stories/BlogPosts";
 import FeaturedStory from "../../components/Stories/FeaturedStory";
 import MoreStories from "../../components/Stories/MoreStories";
 import Layout from "../../layouts/Page";
+import staticPosts from "../../posts.json";
 import { Post } from "../../types/Blog";
 import { Article } from "../../types/Devto";
 import { fetchArticles } from "../../utils/api";
@@ -13,26 +14,6 @@ interface Props {
 }
 
 const Blog = ({ posts }: Props) => {
-  const staticPosts = [
-    {
-      title: "The Founding Story of Rare",
-      description:
-        "“Is there anybody who can code?” Not every company starts its existence from this question, but Rare Agency story resembles something from a classical anecdote. So, there we go.",
-      readable_publish_date: "Oct 25",
-      slug: "founding-story",
-      cover_image: "/static/blog/founding-story/header.jpg",
-      reading_time_minutes: 3,
-      tag_list: "",
-      tags: [],
-      user: {
-        name: "Rare",
-        github_username: "rareagency",
-        website_url: "https://rare.fi",
-        profile_image: "/static/icons/rare-icon-192.png",
-      },
-    },
-  ];
-
   const combinedPosts: (Article | Post)[] = [...posts, ...staticPosts];
 
   return (
