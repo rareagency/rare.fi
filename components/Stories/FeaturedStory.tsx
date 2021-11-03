68db983da807fe23634cf01083d471af26aa88a3
naftis <pyry.rouvila@gmail.com>
2021-11-04 00:13:47 +0200

    Revert "image-container" className

--- components/Stories/FeaturedStory.tsx
+++ components/Stories/FeaturedStory.tsx
@@ -32,7 +32,7 @@ const FeaturedStory = ({ post }: Props) => (
         </p>
 
         <div
-          className="image-container relative md:h-[200px] lg:h-[350px] h-[425px] mt-12 max-w-full sm:h-auto"
+          className="relative md:h-[200px] lg:h-[350px] h-[425px] mt-12 max-w-full sm:h-auto"
           style={{ aspectRatio: "42/17", overflow: "hidden" }}
         >
           {post.cover_image_placeholder ? (
