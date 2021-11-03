6f35acd45cc1491d2d01503a70e3e30f876312d1
naftis <pyry.rouvila@gmail.com>
2021-11-04 00:27:23 +0200

    Revert feature box style testings

--- components/Company/Company.tsx
+++ components/Company/Company.tsx
@@ -4,7 +4,7 @@ import FeatureBox from "./FeatureBox";
 const Company = () => {
   return (
     <section id="company" className="col-all layout-grid p-8 pt-0 md:pt-8">
-      <h1 className="col-start-2 col-span-6 pt-32 font-header-mono md:pt-8 text-center md:text-left">
+      <h1 className="col-start-2 col-span-6 pt-32 font-header-mono md:pt-8">
         Setting pace for the modern tech companies
       </h1>
       <div className="row-start-2 col-all">
--- components/Company/FeatureBox.tsx
+++ components/Company/FeatureBox.tsx
@@ -9,7 +9,7 @@ interface IllustrationProps {
 }
 
 const Illustration = ({ icon, children }: IllustrationProps) => (
-  <div className="flex items-center justify-center h-32">
+  <div className="flex items-center justify-center h-32 ml-[-10px]">
     <figure>{icon}</figure>
     <p className="description leading-6 p-8 w-full sm:pr-0">{children}</p>
   </div>
@@ -18,15 +18,14 @@ const Illustration = ({ icon, children }: IllustrationProps) => (
 const FeatureBox = () => {
   return (
     <section className="layout-grid col-all pt-32 md:pt-8">
-      <h5 className="col-all text-center md:text-left">
+      <h5 className="col-start-2 col-span-1 md:col-span-full">
         Exceptional tech side
       </h5>
-      <p className="paragraph col-start-2 col-span-6 text-center pt-8 md:text-left">
-        We fight mediocrity in development practices and insert ourselves to
-        <br className="md:hidden" />
-        the product teams to tackle projects with confidence, together.
+      <p className="paragraph col-start-4 col-span-4 md:col-span-full">
+        We fight mediocrity in development practices and insert ourselves to the
+        product teams to tackle projects with confidence, together.
       </p>
-      <div className="row-start-3 col-start-2 col-span-6 pt-8 md:row-start-3">
+      <div className="row-start-2 col-start-2 col-span-6 pt-8 md:row-start-3">
         <div className="grid grid-cols-3 grid-rows-auto gap-16 md:gap-8 md:grid-cols-1 xl:grid-cols-2">
           <Illustration
             icon={<Nailing title="Smiling person showing OK-sign" />}
