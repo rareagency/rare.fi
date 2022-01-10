5cbadca4db5205bf51df86cd5912c475f7c29b7c
naftis <pyry.rouvila@gmail.com>
2022-01-10 11:19:31 +0200

    Modify h2 sizes

--- components/Footer/TickerText.tsx
+++ components/Footer/TickerText.tsx
@@ -1,30 +1,23 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
 
-const TickerText = () => {
-  const words = [
-    "collaborative",
-    "accessible",
-    "to last",
-    "with love",
-    "for humans",
-  ];
+const fontClassName =
+  "text-off-white text-[64px] xl:text-[50px] lg:text-[42px] md:text-[35px] sm:text-[1.6rem]";
 
-  return (
-    <ReactTypingEffect
-      text={words}
-      cursorRenderer={(cursor: string) => (
-        <h2 className="text-off-white">{cursor}</h2>
-      )}
-      speed={200}
-      eraseSpeed={50}
-      typingDelay={1000}
-      eraseDelay={3000}
-      displayTextRenderer={(text: string) => {
-        return <h2 className="text-off-white sm:text-[1.6rem]">{text}</h2>;
-      }}
-    />
-  );
-};
+const TickerText = () => (
+  <ReactTypingEffect
+    text={["collaborative", "accessible", "to last", "with love", "for humans"]}
+    cursorRenderer={(cursor: string) => (
+      <h2 className={fontClassName}>{cursor}</h2>
+    )}
+    speed={200}
+    eraseSpeed={50}
+    typingDelay={1000}
+    eraseDelay={3000}
+    displayTextRenderer={(text: string) => (
+      <h2 className={fontClassName}>{text}</h2>
+    )}
+  />
+);
 
 export default TickerText;
