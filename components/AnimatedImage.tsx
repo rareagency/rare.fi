db2a3339ecf837ef47c4279c48b0ace9a98bb167
Marko Leinikka <marko@rare.fi>
2022-01-11 16:14:30 +0200

    refactor: move animation styles to AnimatedImage

--- components/AnimatedImage.tsx
+++ components/AnimatedImage.tsx
@@ -1,16 +1,33 @@
 import Image, { ImageProps } from "next/image";
 import { useState } from "react";
+import { c } from "../utils/classnames";
 
 const AnimatedImage = (props: ImageProps) => {
   const [loaded, setLoaded] = useState(false);
 
   return (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <Image
-      {...props}
-      className={`${props.className} ${loaded ? "unblur" : ""}`}
-      onLoadingComplete={() => setLoaded(true)}
-    />
+    <div>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image
+        {...props}
+        className={c(props.className, loaded ? "unblur" : "")}
+        onLoadingComplete={() => setLoaded(true)}
+      />
+      <style global jsx>{`
+        .unblur {
+          animation: unblur 0.5s linear;
+        }
+
+        @keyframes unblur {
+          from {
+            filter: blur(20px);
+          }
+          to {
+            filter: blur(0);
+          }
+        }
+      `}</style>
+    </div>
   );
 };
 
