b0a877399ab8718538f3be26312afc7fbbb9c190
Marko Leinikka <marko@rare.fi>
2022-01-11 15:05:13 +0200

    fix: code block overflow on Firefox and Safari

--- components/Blog/BlogCode.tsx
+++ components/Blog/BlogCode.tsx
@@ -10,28 +10,16 @@ interface Props {
 
 const BlogCode = ({ className, inline, children }: Props) => {
   const match = /language-(\w+)/.exec(className || "");
-  if (!inline && match) {
+  if (!inline) {
     return (
       <SyntaxHighlighter
-        customStyle={{ fontSize: 16 }}
-        language={match[1] || ""}
+        customStyle={{ fontSize: 16, maxWidth: "85vw" }}
+        language={match && match[1] ? match[1] : ""}
+        gpl
       >
         {/* Removes empty last line */}
         {String(children).replace(/\n$/, "")}
       </SyntaxHighlighter>
-    );
-  }
-
-  if (!inline) {
-    return (
-      <code
-        className={c(
-          "bg-gray-200 px-2 rounded-lg mt-6 block max-w-max overflow-x-auto whitespace-pre md:text-base",
-          className
-        )}
-      >
-        {children}
-      </code>
     );
   }
 
